cd9a17d1f1323a321d19a8f4d89717e5493a6289
Dat Le <thedat.lenguyen@gmail.com>
2015-07-23 16:41:47 +0800

    Avazu: added NN and xgb model.

--- avazu-ctr-prediction/avazu-ctr-prediction.py
+++ avazu-ctr-prediction/avazu-ctr-prediction.py
@@ -11,6 +11,8 @@ from sklearn.lda import LDA
 from sklearn.naive_bayes import GaussianNB
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.preprocessing import StandardScaler, LabelEncoder
+from xgboost import XGBClassifier
+from sknn.mlp import Classifier, Layer
 
 pd.options.mode.chained_assignment = None
 
@@ -69,6 +71,20 @@ for col in ['C18','C20','C21']:
 # Define classifiers
 if sample:
     classifiers = [
+        Classifier(
+            layers=[
+                # Convolution("Rectifier", channels=10, pool_shape=(2,2), kernel_shape=(3, 3)),
+                Layer('Rectifier', units=200),
+                Layer('Softmax')],
+            learning_rate=0.01,
+            learning_rule='momentum',
+            learning_momentum=0.9,
+            batch_size=25,
+            valid_size=0.1,
+            # valid_set=(X_test, y_test),
+            n_stable=10,
+            n_iter=10,
+            verbose=True),
         ExtraTreesClassifier(n_estimators=100),
         RandomForestClassifier(n_estimators=100),
         KNeighborsClassifier(n_neighbors=100, weights='uniform', algorithm='auto', leaf_size=100, p=2, metric='minkowski'),
@@ -76,19 +92,21 @@ if sample:
         GaussianNB(),
         DecisionTreeClassifier(),
         GradientBoostingClassifier(),
-        SGDClassifier(loss='log',n_iter=30,verbose=5,learning_rate='invscaling',eta0=0.0000000001)
+        SGDClassifier(loss='log',n_iter=30,verbose=5,learning_rate='invscaling',eta0=0.0000000001),
+        XGBClassifier(n_estimators=512, max_depth=4)
     ]
 else:
     classifiers = [# Other methods are underperformed yet take very long training time for this data set
         GradientBoostingClassifier(), # takes ~5 hours to train on a 16GB i5 machine.
-        SGDClassifier(loss='log',n_iter=30,verbose=5,learning_rate='invscaling',eta0=0.0000000001)
+        SGDClassifier(loss='log',n_iter=30,verbose=5,learning_rate='invscaling',eta0=0.0000000001),
+        XGBClassifier(n_estimators=512, max_depth=4)
     ]
 
 # Train
 for classifier in classifiers:
     print classifier.__class__.__name__
     start = time.time()
-    classifier.fit(train[list(features)], train.click)
+    classifier.fit(np.array(train[list(features)]), train.click)
     print '  -> Training time:', time.time() - start
 
 # Evaluation and export result
@@ -96,15 +114,15 @@ if sample:
     for classifier in classifiers:
         print classifier.__class__.__name__
         print 'Log Loss:'
-        print log_loss(test.click.values, classifier.predict_proba(test[features]))
+        print log_loss(test.click.values, classifier.predict_proba(np.array(test[features])))
         print 'RMSE:'
-        print mean_squared_error(test.click.values, np.compress([False, True], classifier.predict_proba(test[features]), axis=1))**0.5 # RMSE
+        print mean_squared_error(test.click.values, np.compress([False, True], classifier.predict_proba(np.array(test[features])), axis=1))**0.5 # RMSE
 
 else: # Export result
     for classifier in classifiers:
         if not os.path.exists('result/'):
             os.makedirs('result/')
-        predictions = np.column_stack((test['id'],np.compress([False, True], classifier.predict_proba(test[features]), axis=1)))
+        predictions = np.column_stack((test['id'],np.compress([False, True], classifier.predict_proba(np.array(test[features])), axis=1)))
         csvfile = 'result/' + classifier.__class__.__name__ + '-submit.csv'
         with open(csvfile, 'w') as output:
             writer = csv.writer(output, lineterminator='\n')
