9a12814c839d8a968028c94437bee2ca68801dfd
Dat Le <thedat.lenguyen@gmail.com>
2015-09-04 17:33:16 +0800

    First version. 0.78714

--- springleaf-marketing-response/README.md
+++ springleaf-marketing-response/README.md
@@ -3,4 +3,5 @@ Springleaf Marketing Response
 
 https://www.kaggle.com/c/springleaf-marketing-response
 
-- 1st version: native xgboost. AUC Score = 
+- 1st version: native xgboost. AUC Score = 0.78714 Booster-submit-d8-0.01-mcw3-0.65-0.65-500r
+
--- springleaf-marketing-response/springleaf-xgb-native.py
+++ springleaf-marketing-response/springleaf-xgb-native.py
@@ -1,3 +1,5 @@
+from __future__ import division
+
 import pandas as pd
 import datetime
 import csv
@@ -18,10 +20,15 @@ from sklearn.metrics import roc_auc_score
 from matplotlib import pylab as plt
 
 sample = False
-plot = True # Won't plot if sample = True
 
 goal = 'target'
 myid = 'ID'
+
+# Create random 5000 lines train and test files:
+# brew install coreutils
+# tail -n +2 train.csv | gsort -R | head -5000 > train-5000.csv
+# tail -n +2 test.csv | gsort -R | head -5000 > test-5000.csv
+# rmb to copy over the headers
 
 if not os.path.exists('result/'): os.makedirs('result/')
 
@@ -30,16 +37,28 @@ def load_data():
         Load data and specified features of the data sets
     """
     # Load data
-    train = pd.read_csv('./data/train.csv')
-    test = pd.read_csv('./data/test.csv')
+    if sample:
+        train = pd.read_csv('./data/train-5000.csv')
+        test = pd.read_csv('./data/test-5000.csv')
+    else:
+        train = pd.read_csv('./data/train.csv')
+        test = pd.read_csv('./data/test.csv')
     # Features set
     features = test.columns.tolist()
     numerics = ['int16', 'int32', 'int64', 'float16', 'float32', 'float64']
     features_numeric = test.select_dtypes(include=numerics).columns.tolist()
     features_non_numeric = [f for f in features if f not in features_numeric]
     # # Features set.
-    # noisy_features = [myid,'VAR_0207','VAR_1934','VAR_0493']
     noisy_features = [myid,goal]
+    for f in features:
+        if len(train[f].value_counts()) < 1: # only 0 or 1 class: noisy
+            noisy_features += [f]
+        if sum(train[f].value_counts()) < 0.3 * len(train[f]): # more than 70% is emty
+            print f + ' is ' + str(100 - 100*sum(train[f].value_counts()) / len(train[f])) + ' percent empty...'
+            noisy_features += [f]
+        if len(train[f].value_counts()) > 0.5 * len(train[f]): # too many classes
+            print f + ' has ' + str(len(train[f].value_counts())) + ' classes...'
+            noisy_features += [f]
     features = [c for c in features if c not in noisy_features]
     features_numeric = [c for c in features_numeric if c not in noisy_features]
     features_non_numeric = [c for c in features_non_numeric if c not in noisy_features]
@@ -48,22 +67,19 @@ def load_data():
 def process_data(train,test,features,features_non_numeric):
     train = train.fillna(train.mode().iloc[0])
     test = test.fillna(test.mode().iloc[0])
-    # # filling what wasn't filled yet (wtf / need to fix)
-    for col in features_non_numeric:
-        train[col] = train[col].fillna('-1') # wtf
-        test[col] = test[col].fillna('-1') # wtf
-    train = train.fillna(-1)
-    test = test.fillna(-1)
+    # import pdb;pdb.set_trace()
     # Pre-processing non-numberic values
     le = LabelEncoder()
-    for col in features_non_numeric:
-        print col
+    for col in features:
+        # print col
         le.fit(list(train[col])+list(test[col]))
         train[col] = le.transform(train[col])
         test[col] = le.transform(test[col])
+    # import pdb;pdb.set_trace()
     # Neural Network, Stochastic Gradient Descent is sensitive to feature scaling, so it is highly recommended to scale your data.
     scaler = StandardScaler()
     for col in set(features) - set(features_non_numeric):
+        # print col
         scaler.fit(list(train[col])+list(test[col]))
         train[col] = scaler.transform(train[col])
         test[col] = scaler.transform(test[col])
@@ -71,9 +87,9 @@ def process_data(train,test,features,features_non_numeric):
 
 def XGB_native(train,test,features,features_non_numeric):
     # XGB Params
-    params = {'max_depth':6, 'eta':0.01, 'silent':1,
-              'objective':'multi:softprob', 'num_class':2, 'eval_metric':'mlogloss',
-              'min_child_weight':3, 'subsample':1,'colsample_bytree':0.55, 'nthread':4}
+    params = {'max_depth':8, 'eta':0.01, 'silent':1,
+              'objective':'multi:softprob', 'num_class':2, 'eval_metric':'auc',
+              'min_child_weight':3, 'subsample':0.65,'colsample_bytree':0.65, 'nthread':4}
     num_rounds = 500
     # Training / Cross Validation
     if sample:
@@ -93,42 +109,27 @@ def XGB_native(train,test,features,features_non_numeric):
         print str(datetime.datetime.now())
         xgbtrain = xgb.DMatrix(train[features], label=train[goal])
         classifier = xgb.train(params, xgbtrain, num_rounds)
-        classifier.save_model('model/xgb.model')
-        # classifier = xgb.Booster(model_file='result/xgb.model')
         csvfile = 'result/' + classifier.__class__.__name__ + '-submit.csv'
         with open(csvfile, 'w') as output:
             predictions = []
             print str(datetime.datetime.now())
             for i in test[myid].tolist():
-              # National ServiceMen always resigned
-              if test[test[myid] == i]['EMPLOYEE_GROUP'].item() == 2:
-                predictions += [[i,1]]
-              else:
                 predictions += [[i,classifier.predict(xgb.DMatrix(test[test[myid]==i][features])).tolist()[0][1]]]
             writer = csv.writer(output, lineterminator='\n')
             writer.writerow([myid,goal])
             writer.writerows(predictions)
             print str(datetime.datetime.now())
         # Feature importance
-        if plot:
-          outfile = open('xgb.fmap', 'w')
-          i = 0
-          for feat in features:
-              outfile.write('{0}\t{1}\tq\n'.format(i, feat))
-              i = i + 1
-          outfile.close()
-          importance = classifier.get_fscore(fmap='xgb.fmap')
-          print importance
-          importance = sorted(importance.items(), key=operator.itemgetter(1))
-          df = pd.DataFrame(importance, columns=['feature', 'fscore'])
-          df['fscore'] = df['fscore'] / df['fscore'].sum()
-          # Plotitup
-          plt.figure()
-          df.plot()
-          df.plot(kind='barh', x='feature', y='fscore', legend=False, figsize=(25, 15))
-          plt.title('XGBoost Feature Importance')
-          plt.xlabel('relative importance')
-          plt.gcf().savefig('Feature_Importance_xgb.png')
+        outfile = open('result/xgb.fmap', 'w')
+        i = 0
+        for feat in features:
+            outfile.write('{0}\t{1}\tq\n'.format(i, feat))
+            i = i + 1
+        outfile.close()
+        importance = classifier.get_fscore(fmap='result/xgb.fmap')
+        importance = sorted(importance.items(), key=operator.itemgetter(1))
+        df = pd.DataFrame(importance, columns=['feature', 'fscore'])
+        df.to_csv('result/importance.csv',index=False)
 
 def main():
     print "=> Loading data - " + str(datetime.datetime.now())
