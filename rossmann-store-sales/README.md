6ec8cff5a40a0d3b1796bea15761591f87d9f0b5
Dat Le <thedat.lenguyen@gmail.com>
2015-10-23 16:25:40 +0800

    Rossmann 3rd 0.11021

--- rossmann-store-sales/README.md
+++ rossmann-store-sales/README.md
@@ -5,3 +5,4 @@ https://www.kaggle.com/c/rossmann-store-sales
 
 - 1st version LB 0.37205: basic xgboost
 - 2nd version LB 0.11764 (local: 0.1169): fine-tuning xgboost 1000s training time
+- 3nd version LB 0.11021 (local: 0.1097): like 2nd version but with log and exp mod
--- rossmann-store-sales/rossmann.py
+++ rossmann-store-sales/rossmann.py
@@ -10,7 +10,7 @@ from xgboost import XGBRegressor
 
 pd.options.mode.chained_assignment = None
 if not os.path.exists('result/'): os.makedirs('result/')
-sample = False
+sample = True
 gridsearch = False
 goal = 'Sales'
 myid = 'Id'
@@ -19,6 +19,8 @@ features = ['Store','DayOfWeek','Date','year','month','day','Open','Promo','Stat
             'CompetitionOpenSinceYear','Promo2','Promo2SinceWeek','Promo2SinceYear','PromoInterval']
 
 useless_features = ['Date','Promo2SinceWeek','PromoInterval','CompetitionOpenSinceYear','CompetitionOpenSinceMonth','Promo2SinceYear']
+
+logexp = True
 
 for v in useless_features:
     features.remove(v)
@@ -52,7 +54,7 @@ if sample: # To train with 75% data
         test_org = pd.read_csv('./data/test.csv',dtype={'StateHoliday':pd.np.string_})
     else:
         df = pd.read_csv('./data/train.csv',dtype={'StateHoliday':pd.np.string_})
-        df['is_train'] = np.random.uniform(0, 1, len(df)) <= .75
+        df['is_train'] = (df['Sales'] * 15 + df['Customers'] * 3) % 17 <= 13 # my pseudo random, deal with it
         train_org, test_org = df[df['is_train']==True], df[df['is_train']==False]
 else:
     # To run with real data
@@ -76,10 +78,14 @@ test['month'] = test['month'].astype(float)
 test['day'] = test.Date.apply(lambda x: x.split('-')[2])
 test['day'] = test['day'].astype(float)
 
-train = train.fillna(-1)
-test = test.fillna(-1)
+train = train.fillna(0)
+test = test.fillna(0)
 
 train = train[train['Open'] == 1] # don't train data with open = 0
+
+# Log and Exp
+if logexp:
+    train['Sales'] = np.log(train['Sales']+1)
 
 for f in train[features]:
     if train[f].dtype=='object':
@@ -88,8 +94,8 @@ for f in train[features]:
         train[f] = lbl.transform(list(train[f].values))
         test[f] = lbl.transform(list(test[f].values))
 
-regressor = XGBRegressor(n_estimators=500, nthread=-1, max_depth=12,
-                   learning_rate=0.15, silent=True, subsample=0.8, colsample_bytree=0.7)
+regressor = XGBRegressor(n_estimators=500, nthread=-1, max_depth=18,
+                   learning_rate=0.05, silent=False, subsample=0.8, colsample_bytree=0.7)
 
 start = time.time()
 if (gridsearch & sample): # only do gridsearch if we run with sampled data.
@@ -97,8 +103,7 @@ if (gridsearch & sample): # only do gridsearch if we run with sampled data.
     gscv = GridSearchCV(regressor, {
         'max_depth': [3, 5, 7, 11, 13, 17, 23],
         'n_estimators': [32, 64, 128, 512, 1024, 2048, 4096],
-        'learning_rate': [0.005,0.01,0.025],
-        'min_child_weight': [1,2,3,4,5],
+        'learning_rate': [0.15],
         'subsample': [0.6,0.7,0.8],
         'colsample_bytree': [0.6,0.7,0.8]},
         verbose=1, n_jobs=2)
@@ -113,7 +118,10 @@ print '  -> Training time:', time.time() - start
 if sample:
     if not gridsearch:
         # Test results
-        print "RMSPE: " + str(rmspe(regressor.predict(np.array(test[features])),test[goal].values))
+        if logexp:
+            print "RMSPE: " + str(rmspe(map(lambda x : np.exp(x)-1, regressor.predict(np.array(test[features]))),test[goal].values))
+        else:
+            print "RMSPE: " + str(rmspe(regressor.predict(np.array(test[features])),test[goal].values))
 else:
     csvfile = 'result/' + regressor.__class__.__name__ + '-submit.csv'
     with open(csvfile, 'w') as output:
@@ -124,7 +132,10 @@ else:
                 predictions += [[i,0]]
             else:
                 # import pdb;pdb.set_trace()
-                predictions += [[i,regressor.predict(np.array(test[test[myid]==i][features]))[0]]]
+                if logexp:
+                    predictions += [[i,np.exp(regressor.predict(np.array(test[test[myid]==i][features]))[0])-1]]
+                else:
+                    predictions += [[i,regressor.predict(np.array(test[test[myid]==i][features])[0])]]
         writer = csv.writer(output, lineterminator='\n')
         writer.writerow([myid,goal])
         writer.writerows(predictions)
